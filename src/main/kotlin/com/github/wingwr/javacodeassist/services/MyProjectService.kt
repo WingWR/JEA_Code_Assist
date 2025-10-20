ff037114312238f80361360960e634bfe3861427
GitHub Action <action@github.com>
2025-10-20 05:42:45 +0000

    Template cleanup

--- src/main/kotlin/com/github/wingwr/javacodeassist/services/MyProjectService.kt
+++ src/main/kotlin/com/github/wingwr/javacodeassist/services/MyProjectService.kt
@@ -1,9 +1,9 @@
-package org.jetbrains.plugins.template.services
+package com.github.wingwr.javacodeassist.services
 
 import com.intellij.openapi.components.Service
 import com.intellij.openapi.diagnostic.thisLogger
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
+import com.github.wingwr.javacodeassist.MyBundle
 
 @Service(Service.Level.PROJECT)
 class MyProjectService(project: Project) {
--- src/main/kotlin/com/github/wingwr/javacodeassist/startup/MyProjectActivity.kt
+++ src/main/kotlin/com/github/wingwr/javacodeassist/startup/MyProjectActivity.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template.startup
+package com.github.wingwr.javacodeassist.startup
 
 import com.intellij.openapi.diagnostic.thisLogger
 import com.intellij.openapi.project.Project
